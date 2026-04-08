76e46dcce54971c8d8ef99862be369320b99159c
end3rguy <end3rguy@gmail.com>
2026-04-08 12:39:03 -0700

    created p5js folder and self portrait demo

---
+++ p5js/self_portrait_1/self_portrait_1.js
@@ -0,0 +1,53 @@
+function setup() {
+createCanvas(600, 600);
+}
+function draw() {
+background(230,200,255);
+strokeWeight(12);
+fill(0);
+ellipse(500, 325, 125, 125);
+ellipse(100, 325, 125, 125);
+fill(255,255,0);
+ellipse(125, 250, 125, 125);
+ellipse(475, 250, 125, 125);
+ellipse(450, 400, 125, 125);
+ellipse(150, 400, 125, 125);
+strokeWeight(0);
+ellipse(500, 325, 113, 113);
+ellipse(100, 325, 113, 113);
+strokeWeight(12);
+fill(255);
+ellipse(300, 300, 300, 300);
+fill(0);
+arc(300, 240, 300, 300,PI,0);
+fill(255,255,0)
+ellipse(300, 155, 100, 80);
+strokeWeight(0);
+fill(0);
+ellipse(300, 120, 50, 50);
+fill(255,255,0)
+ellipse(280, 150, 50, 50);
+ellipse(600-280, 150, 50, 50);
+fill(0);
+strokeWeight(0);
+ellipse(300, 240, 330, 150);
+fill(255);
+ellipse(300, 280, 285, 100);
+strokeWeight(12);
+strokeJoin(ROUND);
+rect(315,299,90,52)
+ellipse(240, 340, 90, 90);
+arc(360, 345, 90, 90,0,PI);
+arc(360, 305, 90, 90,PI,0);
+
+fill(255,128,0)
+rect(160,525,280,280);
+fill(0,0,255)
+rect(160,525,100,280);
+rect(340,525,100,280);
+ellipse(200,525,120,40);
+ellipse(400,525,120,40);
+fill(0,255,0);
+rect(260,525,20,280);
+rect(320,525,20,280);
+}
